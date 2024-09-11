2fe7afb08a748719c00c4882a1ec2855dccc91dd
Fito45 <163039915+Fito45@users.noreply.github.com>
2024-09-11 13:05:37 -0600

    Comentarios y variables en JavaScript

---
+++ 01-Fundamentos/comentarios.js
@@ -0,0 +1,22 @@
+// Comentarios en Linea
+
+/*
+Comentarios en bloque
+
+Para más de una linea de comentarios
+se crea usando el shortcut "Shift+Alt+A".
+
+*/
+
+//la función alert lanza una ventana de alerta.
+
+/*
+No debemos recargar mucho el codigo de comentarios
+los comentarios deben ser concretos 
+no deben ser rebundantes respecto al codigo
+contretos y descriptivos.
+*/
+
+//Verificamos enlace con Index.Html (ejemplo)
+
+alert("Estoy Enlazado!!");
---
+++ 01-Fundamentos/variables.js
@@ -0,0 +1,47 @@
+/*
+Variables
+son contenedores que almancenan datos.
+
+las variables se declaran usando las palabras recervadas
+
+Var -> forma tradicional pero ya no se recomienda
+Let -> forma moderna y recomendada
+Const -> forma moderna pero el dato almacenado no se puede cambiar
+
+Para declarar una variable usamos una palabra recervada
+luego el nombre de la variable seguido de un signo "="
+y un ";".
+
+*/
+
+var nombre= "Fito";
+
+console.log(nombre)
+
+let edad= 20;
+
+console.log(edad)
+
+const vivo= true;
+
+console.log(vivo)
+
+/*
+las variables declaradas con var y let pueden cambiar a lo largo
+del codigo, siempre y cuando su alcance lo permita.
+
+Y las variables declaradas con Const no pueden cambiar.
+
+Para cambiar el valor de una variable:
+
+[nombre variable]= "nuevo valor";
+
+*/
+
+nombre = "Hamburger";
+
+console.log(nombre)
+
+edad = 29;
+
+console.log(edad)
