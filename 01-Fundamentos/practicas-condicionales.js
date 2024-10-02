c7e605e0019cdbbe01461bd0042fd92b349c5b6a
Fito45 <163039915+Fito45@users.noreply.github.com>
2024-10-02 12:43:26 -0600

    esta es mi practica de Javascript si no es correcta repruebeme, no guardo rencor

---
+++ 01-Fundamentos/practicas-condicionales.js
@@ -0,0 +1,15 @@
+/*
+Practicas Condicionales
+
+"Promt"(que edad tienes?);
+*/
+
+let edad = prompt("que edad tienes?");
+
+if (edad >= 18) {
+    alert ("eres mayor de edad, más cerca de tu retiro!!");
+} else if (edad <= 18) {
+    alert ("eres menos de edad, estas morrillo!");
+} else {
+    alert ("eres un alien 100% real no fake!!");
+}
